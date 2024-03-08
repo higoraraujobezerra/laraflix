0596924c47d5811a241ee39b546f7f80f9503aa4
Higor Araujo <higoraraujobezerra@gmail.com>
2024-03-07 23:24:48 -0300

    add_integration_test_delete_categry_use_case

---
+++ tests/Feature/Core/UseCase/Category/DeleteCategoryUseCaseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use Tests\TestCase;
+use App\Models\Category;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+
+class DeleteCategoryUseCaseTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExecute()
+    {
+        $model = new Category();
+        $repository = new CategoryEloquentRepository($model);
+        $useCase = new DeleteCategoryUseCase($repository);
+        $categoryDb = Category::factory()->create();
+        $input = new CategoryInputDto(
+            id: $categoryDb->id
+        );
+
+        $useCase->execute($input);
+
+        $this->assertSoftDeleted($categoryDb);
+    }
+}
